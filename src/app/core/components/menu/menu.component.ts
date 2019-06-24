9d5bda1e92dad291fbed5fedd14d0482b1db66bc
Konrad <konrad.domozych2@wp.pl>
2019-06-24 13:58:44 +0200

    Dispaly menu and fix e2e tests

--- src/app/core/components/menu/menu.component.ts
+++ src/app/core/components/menu/menu.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent implements OnInit, OnChanges, OnDestroy {
 
-  constructor() { }
+  constructor() {
+    // instacja klasy
+    console.log('constructor')
+   }
 
   ngOnInit() {
+      // wyrenderowanie komponentu w DOM
+      console.log('ngOnInit')
+
+  }
+  ngOnChanges(){
+    // komponent zostanie zasilony danymi
+    console.log('ngOnChanges')
+  }
+  ngOnDestroy(){
+    // komponent umiera
+    console.log('ngOnDestroy')
   }
 
 }
